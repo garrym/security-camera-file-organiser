0fab90d2959f23b5defc6b948a10ad9f1ca5ce8f
Garry Marsland <garry@basefour.co.uk>
2018-09-23 08:29:17 +0100

    Directory creation

--- src/SecurityCameraFileOrganiser/Program.cs
+++ src/SecurityCameraFileOrganiser/Program.cs
@@ -13,13 +13,17 @@ namespace SecurityCameraFileOrganiser
             var fileNames = Directory.GetFiles(directory);
             foreach (var fileName in fileNames)
             {
-                var fileInfo = new FileInfo(fileName);
-
-                var datePart = fileInfo.Name.Replace("MDAlarm_", "").Replace(".jpg", "");
+                var datePart = new FileInfo(fileName).Name.Replace("MDAlarm_", "").Replace(".jpg", "");
 
                 if (DateTime.TryParseExact(datePart, "yyyyMMdd-HHmmss", CultureInfo.InvariantCulture, DateTimeStyles.None, out var date))
                 {
-                    Console.WriteLine("Found a date!");
+                    var dateDirectory = $"{date.Year:0000}-{date.Month:00}-{date.Day:00}";
+
+                    var fullDateDirectory = Path.Combine(directory, dateDirectory);
+                    if (!Directory.Exists(fullDateDirectory))
+                    {
+                        Directory.CreateDirectory(fullDateDirectory);
+                    }
                 }
             }
         }
