d26aca9fb2ed720e76e6c255855061ac34c8e003
Garry Marsland <garry@basefour.co.uk>
2018-09-23 08:19:22 +0100

    File iterating and date parsing

--- src/SecurityCameraFileOrganiser/Program.cs
+++ src/SecurityCameraFileOrganiser/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.Globalization;
+using System.IO;
 
 namespace SecurityCameraFileOrganiser
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var directory = ConfigurationManager.AppSettings["ImageDirectory"];
+            var fileNames = Directory.GetFiles(directory);
+            foreach (var fileName in fileNames)
+            {
+                if (DateTime.TryParseExact(fileName, "\\MDAlar\\m_yyyyMMdd-HHmmss", CultureInfo.InvariantCulture, DateTimeStyles.None, out var date))
+                {
+                    Console.WriteLine("Found a date!");
+                }
+
+                //MDAlarm_20170831-203351
+                //2017-08-31
+
+            }
         }
     }
 }
